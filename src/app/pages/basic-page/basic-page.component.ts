611cdb962b9056e8ba358160ca8f632e4a980d3d
Baldo Campos <baldomanage@gmail.com>
2026-01-22 08:37:50 -0800

    refactor: Basic pipes examples LowerCasePipe, UpperCasePipe, LowerCasePipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPageComponent {}
+export default class BasicPageComponent {
+  nameLower = signal('baldo');
+  nameUpper = signal('BALDO');
+  fullName = signal('BaLDo CaMpOS');
+}
