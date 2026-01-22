c81531167ddfaafac1807989feb6e107357d56c8
Baldo Campos <baldomanage@gmail.com>
2026-01-22 10:52:40 -0800

    feat: :sparkles: i18nSelectPipe learned

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Baldo',
+  age: 28,
+  gender: 'male',
+  address: 'Baja California, México',
+};
+
+const client2 = {
+  name: 'Yuney',
+  age: 32,
+  gender: 'female',
+  address: 'sinaloa, México',
+};
 
 @Component({
   selector: 'uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent {}
+export default class UncommonPageComponent {
+  // i18nSelect
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
