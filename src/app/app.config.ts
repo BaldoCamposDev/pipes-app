7a2fe3d715cbcb18c82960ab39ab633808fafea1
Baldo Campos <baldomanage@gmail.com>
2026-01-22 10:24:21 -0800

    feat: :sparkles: Added dynamic locale functionality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { registerLocaleData } from '@angular/common';
 import { routes } from './app.routes';
+
+import localEs from '@angular/common/locales/es-MX';
+import localFr from '@angular/common/locales/fr';
+import { LocaleService } from './pages/services/locale.service';
+
+registerLocaleData(localEs, 'es-MX');
+registerLocaleData(localFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      // useValue: 'es-MX',
+      deps: [LocaleService],
+      useFactory: (localService: LocaleService) => localService.getLocale,
+    },
+  ],
 };
--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, effect, signal } from '@angular/core';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../services/locale.service';
 
 @Component({
   selector: 'basic-page',
@@ -7,6 +8,9 @@ import { Component, effect, signal } from '@angular/core';
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
+  localeService = inject(LocaleService);
+  // currentLocale = signal(inject(LOCALE_ID));
+
   nameLower = signal('baldo');
   nameUpper = signal('BALDO');
   fullName = signal('BaLDo CaMpOS');
@@ -21,4 +25,9 @@ export default class BasicPageComponent {
 
     onCleanUp(() => clearInterval(interval));
   });
+
+  changeLocale(locale: AvailableLocale) {
+    console.log({ locale });
+    this.localeService.changeLocale(locale);
+  }
 }
