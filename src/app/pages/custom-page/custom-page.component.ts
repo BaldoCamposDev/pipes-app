4e1d1b29859ccd2c6e38546ceaaa90eebd2d20df
Baldo Campos <baldomanage@gmail.com>
2026-01-22 11:54:12 -0800

    feat: :sparkles: Learned custom pipe creation

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent {}
+export default class CustomPageComponent {
+  name = signal('Baldo Campos');
+
+  upperCase = signal(true);
+
+  toggleCase() {
+    this.upperCase.set(!this.upperCase());
+  }
+}
