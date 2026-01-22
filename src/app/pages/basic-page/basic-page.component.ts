265549aba0b0fc0c5aa40d43a34dbf595c2d14c2
Baldo Campos <baldomanage@gmail.com>
2026-01-22 07:50:55 -0800

    chore: base pages created

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-basic-page.component',
+  selector: 'basic-page',
   imports: [],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BasicPageComponent { }
+export default class BasicPageComponent {}
