3113edbf08a6bdbdb40be26473296a1966dbf6c8
Baldo Campos <baldomanage@gmail.com>
2026-01-23 11:30:38 -0800

    refactor: :recycle: Created CanFlyPipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/hero.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 
 @Component({
   selector: 'custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
