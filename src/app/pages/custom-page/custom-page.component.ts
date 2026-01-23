abcf5da156b5a82cb1d7f912eaefb8cd208c7235
Baldo Campos <baldomanage@gmail.com>
2026-01-23 11:47:44 -0800

    revert: :recycle: New heroTextColorPipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -3,10 +3,11 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/hero.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { heroTextColorPipe as HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 
 @Component({
   selector: 'custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
