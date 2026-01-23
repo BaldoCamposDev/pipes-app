d9a702bc122d161ff1073b46c1faa19356d38c16
Baldo Campos <baldomanage@gmail.com>
2026-01-23 11:40:01 -0800

    refactor: :recycle: ColorPipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/hero.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
   selector: 'custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
